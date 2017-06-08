1befe53f93f80b2a71488473f69325c9bac50f27
FIKUS0FIN <dmytro.symonenko.ui.2014@lpnu.ua>
2017-06-08 04:38:02 +0300

    destroy

--- SimpleBrowser/Parser/PositioningRules/A.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SimpleBrowser.Parser.PositioningRules
-{
-	class A : BodyElementPositioningRule
-	{
-		// static readonly string[] _allowedParentTags = new [] { "" };
-		public override string TagName { get { return "a"; } }
-	}
-}
